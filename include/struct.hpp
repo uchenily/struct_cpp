4a7ee84eff858e2f04f5a451eff9f8662090cbce
uchenily <uchenily@qq.com>
2024-06-24 17:23:44 +0800

    Add string_literal

--- include/struct.hpp
+++ include/struct.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "struct_cpp/data_view.hpp"
 #include "struct_cpp/string.hpp"
+#include "struct_cpp/string_literal.hpp"
 
 #include "struct_cpp/calcsize.hpp"
 #include "struct_cpp/format.hpp"
