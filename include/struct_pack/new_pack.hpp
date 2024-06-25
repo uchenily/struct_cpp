753b2d0c145d85e9b5c217b283b323306b466edf
uchenily <uchenily@qq.com>
2024-06-25 16:21:13 +0800

    calcsize: copy from origin fork

--- include/struct_pack/new_pack.hpp
+++ include/struct_pack/new_pack.hpp
@@ -42,6 +42,39 @@ namespace detail {
     SET_FORMAT_MODE('!', false, true, false);
 #undef SET_FORMAT_MODE
 
+    // Specifying the Big Endian format
+    template <char FormatChar>
+    struct BigEndianFormat {
+        static_assert(isFormatChar(FormatChar), "Invalid Format Char passed");
+        static constexpr size_t size() {
+            return 0;
+        }
+    };
+#undef BigEndianFormat
+#define SET_FORMAT_CHAR(ch, s, rep_type, native_rep_type)                      \
+    template <>                                                                \
+    struct BigEndianFormat<ch> {                                               \
+        static constexpr size_t size() {                                       \
+            return s;                                                          \
+        }                                                                      \
+        static constexpr size_t nativeSize() {                                 \
+            return sizeof(native_rep_type);                                    \
+        }                                                                      \
+        using RepresentedType = rep_type;                                      \
+        using NativeRepresentedType = native_rep_type;                         \
+    }
+    template <typename Fmt, char FormatChar>
+    using RepresentedType = std::conditional_t<
+        Fmt::isNative(),
+        typename BigEndianFormat<FormatChar>::NativeRepresentedType,
+        typename BigEndianFormat<FormatChar>::RepresentedType>;
+    SET_FORMAT_CHAR('?', 1, bool, bool);
+    SET_FORMAT_CHAR('x', 1, char, char);
+    SET_FORMAT_CHAR('b', 1, int8_t, signed char);
+    SET_FORMAT_CHAR('B', 1, uint8_t, unsigned char);
+    SET_FORMAT_CHAR('c', 1, char, char);
+#undef SET_FORMAT_CHAR
+
     template <auto container>
     struct fmt_string {
         static constexpr auto data() -> const char * {
@@ -125,6 +158,104 @@ namespace detail {
             } else {
                 return new_FormatMode<'@'>{};
             }
+        }
+
+        struct RawFormatType {
+            char           formatChar;
+            size_t         repeat;
+            constexpr auto isString() -> bool {
+                return formatChar == 's';
+            }
+        };
+        struct FormatType {
+            char           formatChar;
+            size_t         formatSize;
+            size_t         size;
+            constexpr auto isString() -> bool {
+                return formatChar == 's';
+            }
+        };
+        constexpr auto doesFormatAlign(FormatType format) -> bool {
+            return format.formatSize > 1;
+        }
+        template <typename Fmt, char FormatChar, size_t Repeat = 1>
+        constexpr auto getSize() -> size_t {
+            if constexpr (format_mode().is_native()) {
+                return BigEndianFormat<FormatChar>::nativeSize() * Repeat;
+            } else {
+                return BigEndianFormat<FormatChar>::size() * Repeat;
+            }
+        }
+
+        template <size_t Item, typename Fmt, size_t... Is>
+        constexpr auto getTypeOfItem(std::index_sequence<Is...> /*unused*/)
+            -> RawFormatType {
+            constexpr char fomratString[] = {Fmt::at(Is)...};
+            RawFormatType  wrappedTypes[countItems(Fmt{})]{};
+            size_t         currentType = 0;
+            for (size_t i = 0; i < sizeof...(Is); i++) {
+                if (is_format_mode(fomratString[i])) {
+                    continue;
+                }
+                auto repeatCount = consume_number(fomratString, i);
+                i = repeatCount.second;
+                wrappedTypes[currentType].formatChar = fomratString[i];
+                wrappedTypes[currentType].repeat = repeatCount.first;
+                if (repeatCount.first == 0) {
+                    wrappedTypes[currentType].repeat = 1;
+                }
+                currentType++;
+            }
+            return getUnwrappedItem<Item>(wrappedTypes);
+        }
+
+        template <size_t Item, typename Fmt>
+        constexpr auto getTypeOfItem() -> FormatType {
+            static_assert(Item < count_items(),
+                          "Item requested must be inside the format");
+            constexpr RawFormatType format = getTypeOfItem<Item, Fmt>(
+                std::make_index_sequence<Fmt::size()>());
+            constexpr FormatType type
+                = {format.formatChar,
+                   getSize<Fmt, format.formatChar>(),
+                   getSize<Fmt, format.formatChar, format.repeat>()};
+            return type;
+        }
+
+        template <typename Fmt, size_t... Items>
+        constexpr auto getBinaryOffset(std::index_sequence<Items...> /*unused*/)
+            -> size_t {
+            constexpr FormatType itemTypes[] = {getTypeOfItem<Items>(Fmt{})...};
+            constexpr auto       formatMode = format_mode();
+            size_t               size = 0;
+            for (size_t i = 0; i < sizeof...(Items) - 1; i++) {
+                size += itemTypes[i].size;
+                if (formatMode.shouldPad()) {
+                    if (doesFormatAlign(itemTypes[i + 1])) {
+                        auto currentAlignment
+                            = (size % itemTypes[i + 1].formatSize);
+                        if (currentAlignment != 0) {
+                            size += itemTypes[i + 1].formatSize
+                                    - currentAlignment;
+                        }
+                    }
+                }
+            }
+            return size;
+        }
+
+        template <size_t Item, typename Fmt>
+        constexpr auto getBinaryOffset() -> size_t {
+            return getBinaryOffset(std::make_index_sequence<Item + 1>());
+        }
+
+        // https://docs.python.org/3/library/struct.html#struct.calcsize
+        constexpr auto calcsize() -> std::size_t {
+            // 3H4B10s:
+            // count_items: 3*4 + 4 * 1 + 10
+            constexpr auto num_items = count_items();
+            constexpr auto last_item = getTypeOfItem<num_items - 1>();
+            return getBinaryOffset<num_items - 1>() + last_item.size;
         }
     };
 
