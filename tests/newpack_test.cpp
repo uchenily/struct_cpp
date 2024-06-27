70c1d40eebbc36e2e94e92f83ee77da4178e5c3b
uchenily <uchenily@qq.com>
2024-06-27 10:33:20 +0800

    pack 2

--- tests/newpack_test.cpp
+++ tests/newpack_test.cpp
@@ -1,5 +1,10 @@
 #include "struct_pack.hpp"
+#include "struct_pack/debug.hpp"
 
 auto main() -> int {
-    struct_pack::new_pack<"!BB">(0x12, 0x34);
+    auto packed = struct_pack::new_pack<"!BB">(0x12, 0x34);
+    PRINT("t[0] = 0x{:02x}", packed[0]);
+    PRINT("t[1] = 0x{:02x}", packed[1]);
+    // ASSERT(t[0] == 0x12);
+    // ASSERT(t[1] == 0x34);
 }
